082902aefaf6d42cf2f1bc65d15db175d52540d1
Alix Tiran-Cappello <alix.tiran-cappello@laposte.net>
2021-08-14 13:07:10 +0200

    Add function to plot all type of data split between fluids

--- renault/plot.py
+++ renault/plot.py
@@ -23,5 +23,27 @@ def plot_selective_fluid(df, pji_numer, fluid, measure):
     """visualize the data for a given PJI, fluid, and measurement"""
     temp = df[df['fluid'] == fluid]
     temp = temp[(temp['pji'] == pji_numer) & (temp['measurement'] == measure)]
+    temp.reset_index(drop= True, inplace=True)
+    temp.reset_index(inplace=True)
     
-    pass
+    return sns.scatterplot(data=temp, x='index', y='dataValue')
+
+
+def plot_fluids(df, x, y):
+    """Plot a scatter plot for x and y;
+    with separate suplots for each fluid"""
+    
+    fig, ax = plt.subplots(1, 3, figsize= (14, 5))
+    temp = df[df['fluid'] ==  'FRFluid']
+    sns.scatterplot(data=temp, x=x, y=y, ax=ax[0], color='g')
+    ax[0].set_title('FRFluid')
+
+    temp = df[df['fluid'] == 'HFOFluid']
+    sns.scatterplot(data=temp, x=x, y=y,  ax=ax[1], color='b')
+    ax[1].set_title('HFOFluid')
+
+    temp = df[df['fluid'] == 'RMFluid']
+    sns.scatterplot(data=temp, x=x, y=y,  ax=ax[2], color='r')
+    ax[2].set_title('RMFluid')
+    
+    return fig, ax
